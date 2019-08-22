e382d9c6cc885b10c336e25753712874350bfdb4
saadbhai <sourcingo94@gmail.com>
2019-08-22 21:17:29 +0500

    oky man you are a good boy

---
+++ src/app/formpage/formpage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-formpage',
+  templateUrl: './formpage.component.html',
+  styleUrls: ['./formpage.component.scss']
+})
+export class FormpageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
